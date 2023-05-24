de1302d726c482f95500d96639eeb5cfa76a7331
23Djabrail95 <126778925+23Djabrail95@users.noreply.github.com>
2023-05-24 18:44:25 +0300

    Update Bot.py

--- Test/Bot.py
+++ Test/Bot.py
@@ -1,2 +1,27 @@
-print("Hello, World!")
-print("Начинается код")
+
+
+import telebot
+from googletrans import Translator
+from langdetect import detect
+
+
+translator = Translator()
+bot = telebot.TeleBot('6230759452:AAEGZzEofebdMv00PS1jnFmWn2FUkBa3hzM')
+
+@bot.message_handler(func=lambda m: True)
+def translate_message(message):
+
+  src = detect(message.text)
+
+
+  dest = 'ru'
+  
+
+
+  translated_text = translator.translate(message.text, src=src, dest=dest).text
+
+
+  bot.send_message(message.chat.id, translated_text)
+
+
+bot.polling(none_stop=True)
