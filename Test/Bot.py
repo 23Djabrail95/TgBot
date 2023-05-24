e10491f00efb8a1ac186e1db043065e08da8104d
23Djabrail95 <126778925+23Djabrail95@users.noreply.github.com>
2023-05-24 18:57:41 +0300

    Update Bot.py

--- Test/Bot.py
+++ Test/Bot.py
@@ -1,27 +1,24 @@
-
-
 import telebot
-from googletrans import Translator
-from langdetect import detect
 
+from googletrans import Translator
 
 translator = Translator()
 bot = telebot.TeleBot('6230759452:AAEGZzEofebdMv00PS1jnFmWn2FUkBa3hzM')
 
+
+# Определяем функцию для обработки сообщений
 @bot.message_handler(func=lambda m: True)
 def translate_message(message):
+    # Определяем язык исходного текста
+    src_lang = translator.detect(message.text).lang
+    # Задаем целевой язык перевода
+    dest_lang = 'ru' if src_lang == 'en' else 'en'
 
-  src = detect(message.text)
-
-
-  dest = 'ru'
-  
-
-
-  translated_text = translator.translate(message.text, src=src, dest=dest).text
-
-
-  bot.send_message(message.chat.id, translated_text)
+    # Переводим полученное сообщение на целевой язык
+    translated_text = translator.translate(message.text, src=src_lang, dest=dest_lang).text
+    # Отправляем переведенное сообщение
+    bot.reply_to(message, translated_text)
 
 
+# Запускаем бота
 bot.polling(none_stop=True)
