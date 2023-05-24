84944b3a4c3a103a0eb535a50da4dec62422f751
23Djabrail95 <126778925+23Djabrail95@users.noreply.github.com>
2023-05-25 00:59:53 +0300

    Update Bot.py

---
+++ Test/Bot.py
@@ -0,0 +1,15 @@
+import telebot
+from googletrans import Translator
+
+translator = Translator()
+bot = telebot.TeleBot('5970069277:AAEGlfiW4MF4F4wJgO8pr1qVx_GZczvpGoA')
+
+
+# Определяем функцию для обработки сообщений
+@bot.message_handler(func=lambda m: True)
+def translate_message(message):
+    # Определяем язык исходного текста
+    src_lang = translator.detect(message.text).lang
+    # Задаем целевой язык перевода
+    dest_lang = 'ru' if src_lang == 'en' else 'en'
+
