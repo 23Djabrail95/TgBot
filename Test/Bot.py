993730aba78a02575b03f2a17c0d8156f1ff8fc8
23Djabrail95 <126778925+23Djabrail95@users.noreply.github.com>
2023-05-25 00:39:45 +0300

    Update Bot.py

---
+++ Test/Bot.py
@@ -0,0 +1,15 @@
+
+if message.text == '/dev':
+    bot.send_message(message.chat.id, 'Разработчики:\nГадаев Салахь \nТимаев Джабраил\nБалатпиев Ибрагим')
+elif message.text == '/teacher':
+    bot.send_message(message.chat.id, 'Преподователь: \n Пахаев Хусен')
+elif message.text == '/help':
+    bot.send_message(message.chat.id, 'Бот может перевести введенный текст с Русского на Английский или наоборот ')
+    # ПереОодим полученное сообщение на целевой язык
+translated_text = translator.translate(message.text, src=src_lang, dest=dest_lang).text
+
+# Отправляем переведенное сообщение
+bot.reply_to(message, translated_text)
+
+# Запускаем бота
+bot.polling(none_stop=True)
